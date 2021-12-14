da50c7f5e28f80da3734c975821f545944fdab77
Yazeed AlKhalaf <yazeedfady@gmail.com>
2021-12-14 16:51:34 +0300

    ➕ Add counter ui

---
+++ lib/screens/counter_screen.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class CounterScreen extends StatelessWidget {
+  const CounterScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Counter Screen",
+        ),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                "Use the buttons below to\nmanipulate the counter:",
+                style: TextStyle(
+                  fontSize: 24,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 15),
+              const Text(
+                // TODO: replace this with the state from the counter bloc.
+                "0",
+                style: TextStyle(
+                  color: Colors.black54,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 48,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 15),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  IconButton(
+                    onPressed: () {
+                      // TODO: add decrement event to counter bloc.
+                    },
+                    icon: const Icon(
+                      Icons.remove_rounded,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      // TODO: add reset event to counter bloc.
+                    },
+                    icon: const Icon(
+                      Icons.restore_rounded,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      // TODO: add increment event to counter bloc.
+                    },
+                    icon: const Icon(
+                      Icons.add_rounded,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
